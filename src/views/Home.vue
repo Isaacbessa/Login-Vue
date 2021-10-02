eda3cc15e47647ba572c52022887d72ee040a683
Isaacbessa <isaacbessa01@gmail.com>
2021-10-02 14:22:42 -0300

    login 1.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,8 @@
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
                         <h1
-                          class="text-center display-2 teal--text text--accent-3"
-                        >Sign in to Diprella</h1>
+                          class="text-center display-2 black--text text--accent-3 font-family:sans-serif"
+                        >Login </h1>
                         <div class="text-center mt-4">
                           <v-btn class="mx-2" fab color="black" outlined>
                             <v-icon>fab fa-facebook-f</v-icon>
@@ -25,61 +25,70 @@
                             <v-icon>fab fa-linkedin-in</v-icon>
                           </v-btn>
                         </div>
-                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                        <h4 class="text-center mt-4">Garanta seu e-mail e senha para login</h4>
                         <v-form>
                           <v-text-field
                             label="Email"
                             name="Email"
                             prepend-icon="email"
                             type="text"
-                            color="teal accent-3"
+                            color="black accent-3"
                           />
 
                           <v-text-field
                             id="password"
-                            label="Password"
-                            name="password"
+                            label="Senha"
+                            name="Senha"
                             prepend-icon="lock"
                             type="password"
-                            color="teal accent-3"
+                            color="black accent-3"
                           />
                         </v-form>
-                        <h3 class="text-center mt-4">Forgot your password ?</h3>
                       </v-card-text>
+
                       <div class="text-center mt-3">
-                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                        <v-btn rounded color="black accent-3" dark>ENTRAR</v-btn>
                       </div>
                     </v-col>
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Hello, Friend!</h1>
+                    <v-col cols="12" md="4" class="black accent-3">
+                      <v-card-text class="white--text mt-11">
+                        <h1 class="text-center display-1">Bem vindo!</h1>
                         <h5
                           class="text-center"
-                        >Enter your personal details and start journay with us</h5>
+                        >Insira seus dados pessoais e comece a jornada conosco 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+                        </svg>
+                          </h5>
                       </v-card-text>
                       <div class="text-center">
-                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                        <v-btn rounded outlined dark @click="step++">Inscreva-se
+                        </v-btn>
                       </div>
                     </v-col>
                   </v-row>
                 </v-window-item>
                 <v-window-item :value="2">
                   <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="teal accent-3">
+                    <v-col cols="12" md="4" class="black accent-3">
                       <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Welcome Back!</h1>
+                        <h1 class="text-center display-1">Bem-Vindo!</h1>
                         <h5
                           class="text-center"
-                        >To Keep connected with us please login with your personnel info</h5>
+                        >Para se manter conectado conosco, faça o login com suas informações pessoais
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+                        </svg>
+                        </h5>
                       </v-card-text>
                       <div class="text-center">
-                        <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
+                        <v-btn rounded outlined dark @click="step--">Entrar</v-btn>
                       </div>
                     </v-col>
 
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
+                        <h1 class="text-center display-2 black--text text--accent-3 ">Criar conta</h1>
                         <div class="text-center mt-4">
                           <v-btn class="mx-2" fab color="black" outlined>
                             <v-icon>fab fa-facebook-f</v-icon>
@@ -92,21 +101,21 @@
                             <v-icon>fab fa-linkedin-in</v-icon>
                           </v-btn>
                         </div>
-                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                        <h4 class="text-center mt-4">Seu e-mail para cadastro e crie sua senha</h4>
                         <v-form>
                           <v-text-field
-                            label="Name"
-                            name="Name"
+                            label="Nome"
+                            name="Nome"
                             prepend-icon="person"
                             type="text"
-                            color="teal accent-3"
+                            color="black accent-3"
                           />
                           <v-text-field
                             label="Email"
                             name="Email"
                             prepend-icon="email"
                             type="text"
-                            color="teal accent-3"
+                            color="black accent-3"
                           />
 
                           <v-text-field
@@ -115,12 +124,12 @@
                             name="password"
                             prepend-icon="lock"
                             type="password"
-                            color="teal accent-3"
+                            color="black accent-3"
                           />
                         </v-form>
                       </v-card-text>
                       <div class="text-center mt-n5">
-                        <v-btn rounded color="teal accent-3" dark>SIGN UP</v-btn>
+                        <v-btn rounded color="teal accent-3" dark>Cadastrar</v-btn>
                       </div>
                     </v-col>
                   </v-row>
@@ -144,3 +153,7 @@ export default {
   }
 };
 </script>
+
+<style>
+h1{font-family: monospace;}
+</style>
